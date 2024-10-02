5e90e18fdbab122fb45d0a7b34a13dabe4ab3e58
Felipe Coste <32721311+felipecoste@users.noreply.github.com>
2024-10-02 19:15:03 +0000

    Se modifica app.js para hacer variable el HTML y se agrega color del fondo

--- src/js/app.js
+++ src/js/app.js
@@ -30,17 +30,41 @@ function render(variables = {}) {
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
+  document.documentElement.style.setProperty(
+    "--color-fondo",
+    `${variables.bgColorCard}`
+  );
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name ? variables.name : "Lucy"} ${
+    variables.lastName ? variables.lastName : "Boilett"
+  }</h1>
+          <h2>${variables.role ? variables.role : "No role yet"}</h2>
+          <h3>${variables.city ? variables.city : "City"}, ${
+    variables.country ? variables.country : "Country"
+  }</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="${
+              variables.twitter
+                ? variables.twitter
+                : "https://twitter.com/4geeksacademy"
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${
+              variables.github
+                ? variables.github
+                : "https://github.com/4geeksacademy"
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="${
+              variables.linkedin
+                ? variables.linkedin
+                : "https://linkedin.com/school/4geeksacademy"
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${
+              variables.instagram
+                ? variables.instagram
+                : "https://instagram.com/4geeksacademy"
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -68,6 +92,7 @@ window.onload = function() {
     lastName: null,
     role: null,
     country: null,
+    bgColorCard: null,
     city: null
   };
   render(window.variables); // render the card for the first time
